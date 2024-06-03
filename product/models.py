6d99db9508e1991553609814609860f0860cdead
rhtm123 <maurya.iitk@gmail.com>
2024-06-03 18:57:31 +0530

    current_price added in productVariant

--- product/models.py
+++ product/models.py
@@ -42,6 +42,7 @@ class ProductVariant(models.Model):
 
     name = models.CharField(max_length=255, null=True, blank=True)
     mrp = models.DecimalField(max_digits=10, decimal_places=2) # MRP 
+    current_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
 
     def __str__(self):
         return f'{self.product.name} {self.name}'
